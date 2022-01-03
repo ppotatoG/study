1f77f4e53369989cbb04fc31045ed95e0bf74a73
ppotatoG <thgus2270@gmail.com>
2022-01-03 23:40:55 +0900

    Add type restrictions

--- typescript/week2/generics.js
+++ typescript/week2/generics.js
@@ -10,3 +10,25 @@ var obj = {
     value: 'hi',
     selected: true
 };
+// 타입 제한
+var logTextLength = function (text) {
+    console.log(text.length);
+    text.forEach(function (text) {
+        console.log(text);
+    });
+    return text;
+};
+logTextLength(['hi', 'abc']);
+var logTextLength2 = function (text) {
+    text.length;
+    return text;
+};
+logTextLength2('10');
+;
+var getShoppingItemOption = function (itemOption) {
+    return itemOption;
+};
+// error
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption('name');
--- typescript/week2/generics.ts
+++ typescript/week2/generics.ts
@@ -17,3 +17,43 @@ const obj : Dropdowon<string> = {
     value : 'hi',
     selected : true
 };
+
+// 타입 제한
+const logTextLength = <T extends{}>(text : T[]) : T[] => {
+    console.log(text.length);
+    text.forEach(text => {
+        console.log(text);
+    })
+    return text;
+}
+
+logTextLength<string>(['hi', 'abc']);
+
+// 정의된 타입으로 타입 제한
+interface LengthType {
+    length : number;
+}
+
+const logTextLength2 = <T extends LengthType>(text : T) : T => {
+    text.length;
+    return text;
+}
+
+logTextLength2('10');
+
+// keyof으로 타입 제한
+interface ShoppingItem {
+    name : string,
+    price : number,
+    stock : number
+};
+
+const getShoppingItemOption = <T extends keyof ShoppingItem>(itemOption : T) : T => {
+    return itemOption;
+}
+
+// error
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+
+getShoppingItemOption('name');
