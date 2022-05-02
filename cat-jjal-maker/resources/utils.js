58d7133b04414224105e4c5796088524ad188542
ppotatoG <thgus2270@gmail.com>
2022-05-02 11:13:40 +0900

    인트로 - 코드 정리

--- cat-jjal-maker/resources/utils.js
+++ cat-jjal-maker/resources/utils.js
@@ -8,17 +8,17 @@ const CAT3 = "https://cataas.com/cat/595f280b557291a9750ebf65/says/JavaScript";
 const includesHangul = (text) => /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/i.test(text);
 
 const jsonLocalStorage = {
-  setItem: (key, value) => {
-    localStorage.setItem(key, JSON.stringify(value));
-  },
-  getItem: (key) => {
-    return JSON.parse(localStorage.getItem(key));
-  },
+    setItem: (key, value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+    getItem: (key) => {
+        return JSON.parse(localStorage.getItem(key));
+    },
 };
 
 const fetchCat = async (text) => {
-  const OPEN_API_DOMAIN = "https://cataas.com";
-  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
-  const responseJson = await response.json();
-  return `${OPEN_API_DOMAIN}/${responseJson.url}`;
+    const OPEN_API_DOMAIN = "https://cataas.com";
+    const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
+    const responseJson = await response.json();
+    return `${OPEN_API_DOMAIN}/${responseJson.url}`;
 };
